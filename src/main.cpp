4ded65e35f6ae5e16ac198035cb542a80f2660a2
mukundrao9 <59066247+mukundrao9@users.noreply.github.com>
2020-11-02 03:27:24 -0600

    changed main to create a Board with a given fen

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,13 @@
 #include <stdio.h>
 #include <iostream>
 #include "../include/board.hpp"
+#include <string>
 
 
 void printBoard(Board *b);
 
 int main(){
-    Board *board = new Board();
+    ;
+    Board *board = new Board("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1");
     return 0;
 }
